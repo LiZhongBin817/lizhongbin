9df021c024caae3135378ed33a9729384d726b73
qianxi67 <zhengkai19970818@outlook.com>
2019-07-26 10:35:10 +0800

    第二次修改Sys_UserInfo

--- CDWM_MR.Model/Models/Sys_UserInfo.cs
+++ CDWM_MR.Model/Models/Sys_UserInfo.cs
@@ -100,6 +100,9 @@ namespace CDWM_MR.Model.Models
         [SugarColumn(Length = int.MaxValue, IsNullable = true)]
         public DateTime LastLoginTime { get; set; } = DateTime.Now;
 
+        /// <summary>
+        /// 分配的角色名字
+        /// </summary>
         [SugarColumn(IsNullable = true,IsIgnore =true)]
         public List<string> RoleNames { get; set; }
     }
