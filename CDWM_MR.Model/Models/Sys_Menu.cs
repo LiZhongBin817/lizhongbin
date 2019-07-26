afad646b0dcd3f12412a97d9b0e8d963192d4a3e
qianxi67 <zhengkai19970818@outlook.com>
2019-07-26 11:09:25 +0800

    delete file

--- CDWM_MR.Model/Models/Sys_Menu.cs
+++ CDWM_MR.Model/Models/Sys_Menu.cs
@@ -5,15 +5,7 @@ using System.Text;
 
 namespace CDWM_MR.Model.Models
 {
-    /// <summary>
-    /// 菜单表
-    /// </summary>
-    public class Sys_Menu:BaseModel
+    public class Sys_Menu
     {
-        /// <summary>
-        /// 菜单名称
-        /// </summary>
-        [SugarColumn(IsNullable = true,Length = 80)]
-        public string MenuName { get; set; }
     }
 }
