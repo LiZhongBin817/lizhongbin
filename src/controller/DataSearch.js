3f8a477fe392db3fb47a2326431afaccd0c15ef4
xtdf <3070137681@qq.com>
2019-10-13 20:03:31 +0800

    pyb前端

---
+++ src/controller/DataSearch.js
@@ -0,0 +1,134 @@
+﻿/*Title:抄表数据管理
+ * Creator:彭跃彪
+ * Date:2019.9.21
+ */
+layui.define(['table', 'view', 'form', 'admin', 'laydate'], function (exports) {
+    var table = layui.table
+        , view = layui.view
+        , form = layui.form
+        , $ = layui.$
+        , load = layer.load(3)
+        , admin = layui.admin
+        , laydate = layui.laydate
+
+    //初始化
+
+
+
+    //表格渲染
+    table.render({
+        elem: '#dataManageInfo_Table',
+        method: 'post',
+        url: 'http://localhost:8081/ShowDataSearchInfo',
+        cols: [[
+            { field: 'id', title: '序号', width: 110 },
+            { field: 'account', title: '户号', width: 110 },
+            { field: 'username', title: '户名', width: 110 },
+            { field: 'meternum', title: '表号', width: 110 },
+            { field: 'regionname', title: '区域', width: 110 },
+            { field: 'areaname', title: '小区', width: 110 },
+            { field: 'telephone', title: '联系电话', width: 110 },
+            { field: 'meterbooknumber', title: '抄表册', width: 110 },
+            { field: 'address', title: '地址', width: 110 },
+            { field: 'mrreadername', title: '抄表员', width: 110 },
+            { field: 'readDateTime', title: '抄表月份', width: 110 },
+            { field: 'lastmonthdata', title: '上月读数', width: 110 },
+            { field: 'nowmonthdata', title: '本月读数', width: 110 },
+            { field: 'usewaternum', title: '本月用量', width: 110 },
+            { field: 'omrdatetime', title: '抄表时间', width: 110 },
+            {
+                field: 'readtype', title: '抄表状态', width: 110,
+                templet: function (d) {
+                    var intValue = "";
+                    if (d.readtype == null) {
+                        intValue = "未抄";
+                    }
+                    else if (d.readtype == 1) {
+                        intValue = "已抄";
+                    }
+                    else if (d.readtype == 2) {
+                        intValue = "估抄";
+                    }
+                    else {
+                        intValue = "异常";
+                    }
+                    return intValue;
+                }
+            },
+            { field: 'meterstatus', title: '表况', width: 110 },
+            { field: ' ', title: '图片', width: 110 },
+
+        ]]
+        , page: true
+        , toolbar: true
+        , limit: 20
+        , limits: [20, 30, 40]
+        , done: function () {
+            layer.close(load);
+        }
+    });
+    //下拉框渲染
+
+    admin.req({
+        url: 'http://localhost:8081/render_regionInfo',
+        type: "post",
+        success: function (result) {
+            workData = result.data;
+            var strs = "";
+            strs += '<option value = "">请选择</option>'
+            for (var x in workData) {
+                strs += '<option value = "' + workData[x] + '">' + workData[x] + '</option>'
+            }
+            $("#regionname").html(strs);
+            form.render();
+        }
+    });
+
+
+
+    //监听区域下拉框
+    form.on('select(regionnames)', function (d) {
+        var nos = $("#regionname").val();
+        console.log(nos);
+        admin.req({
+            url: 'http://localhost:8081/renderdataInfo',
+            type: 'post',
+            data: {
+                'JsonData': nos
+            },
+            success: function (d) {
+                data = d.data;
+                var strs = "";
+                for (var x in data) {
+                    strs += '<option value = "' + data[x] + '">' + data[x] + '</option>'
+                }
+                $("#areaname").html(strs);
+                form.render();
+
+            }
+
+
+        });
+    });
+    //监听查询
+    form.on('submit(numSearch)', function (obj) {
+        var field = obj.field;
+        table.reload('dataManageInfo_Table', {
+            where: {
+                'account': field.account,
+                'username': field.username,
+                'meternum': field.meternum,
+                'telephone': field.telephone,
+                'meterbooknumber': field.meterbooknumber,
+                'mrreadername': field.mrreadername,
+                'ordatatime01': field.ordatatime01,
+                'ordatatime02': field.ordatatime02,
+                'regionname': field.regionname,
+                'areaname': field.areaname,
+                'page': 1
+            }
+        });
+    });
+
+    exports('DataSearch', {});
+});
---
+++ src/controller/ReadAnalysis.js
@@ -0,0 +1,226 @@
+﻿
+/*Title:抄表数据管理
+ * Creator:彭跃彪
+ * Date:2019.10.08
+ */
+layui.define(['table', 'view', 'form', 'admin', 'laydate', 'echarts', 'carousel'], function (exports) {
+    var table = layui.table
+        , view = layui.view
+        , form = layui.form
+        , $ = layui.$
+        , load = layer.load(3)
+        , admin = layui.admin
+        , laydate = layui.laydate
+        , carousel = layui.carousel
+        , device = layui.device();
+
+
+
+    //单个柱状图渲染
+    function singlerender(o) {
+        //标准柱状图
+        var echnormcol = [], normcol = [
+            {
+                title: {
+                    text: '抄表个数与时间量化统计',
+                    subtext: '常德水表厂'
+                },
+                tooltip: {
+                    trigger: 'axis'
+                },
+                legend: {
+                    data: ['抄表个数', '抄表时长']
+                },
+                calculable: true,
+                xAxis: [
+                    {
+                        type: 'category',
+                        data: o.readtime
+                    }
+                ],
+                yAxis: [
+                    {
+                        type: 'value'
+                    }
+                ],
+                series: [
+                    {
+                        name: '抄表个数',
+                        type: 'bar',
+                        data: o.readnum,
+                        markPoint: {
+                            data: [
+                                { type: 'max', name: '最大值' },
+                                { type: 'min', name: '最小值' }
+                            ]
+                        },
+                        markLine: {
+                            data: [{ type: 'average', name: '平均值' }]
+                        }
+                    },
+                    {
+                        name: '抄表时长',
+                        type: 'bar',
+                        data: o.metertime,
+                        markPoint: {
+                            data: [
+                                 
+                            ]
+                        },
+                        markLine: {
+                            data: [
+                                { type: 'average', name: '平均值' }
+                            ]
+                        }
+                    }
+                ]
+            }
+        ]
+            , elemNormcol = $('#LAY-index-normcol' + o.readerid).children('div')
+            , renderNormcol = function (index) {
+                echnormcol[index] = echarts.init(elemNormcol[index], layui.echartsTheme);
+                echnormcol[index].setOption(normcol[index]);
+                window.onresize = echnormcol[index].resize;
+            };
+        if (!elemNormcol[0]) return;
+        renderNormcol(0);
+        //console.log(renderNormcol);
+    }
+    //初始化
+    function ShowPage(data, num) {
+        var alist = {
+            readerid: 0,
+            readmonth: [],
+            readnum: [],
+            readname: "",
+            readtime: [],
+            metertime: []
+        };
+        var sumlist = [];
+        for (var i = 0; i < num; i++) {
+            var temp = data[i], temp2 = i === data.length - 1 ? { id: null } : data[i + 1];
+            alist.readmonth.push(temp.metermonth ? temp.metermonth : "undefind");
+            alist.readnum.push(temp.meternum);
+            alist.readtime.push(temp.readtime ? temp.readtime : "undefind");
+            alist.metertime.push(temp.readmetertime);
+            if (temp.id !== temp2.id) {
+                alist.readerid = temp.id;
+                alist.readname = temp.mrreadername;
+                sumlist.push(alist);
+                alist = {
+                    readerid: 0,
+                    readname: "",
+                    readmonth: [],
+                    readnum: [],
+                    readtime: [],
+                    metertime: []
+                }
+            }
+        }
+        return sumlist;
+    }
+
+    function renderhtml(d) {
+        var sumhtml = ``;
+        for (var i = 0; i < d.length; i++) {
+            var htm = `<div class="layui-card">
+                    <div class="layui-card-header">${d[i].readname}</div>
+                    <div class="layui-card-body">
+                        <div class="layui-carousel layadmin-carousel layadmin-dataview" data-anim="fade" lay-filter="LAY-index-normcol">
+                            <div carousel-item id="LAY-index-normcol${d[i].readerid}">
+                                <div><i class="layui-icon layui-icon-loading1 layadmin-loading"></i></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>`;
+            sumhtml += htm;
+        }
+        $("#Histogramcontainer").html(sumhtml);
+    }
+    //表格渲染
+    table.render({
+        elem: '#dataAnalysisInfo_Table',
+        method: 'post',
+        toolbar: true,
+        url: 'http://localhost:8081/ReadAnalysis',
+        cols: [[
+
+            { title: '序号', type: 'numbers', width: 70, totalRowText: '合计' },
+            { field: 'mrreadername', title: '抄表员', width: 110 },
+            { field: 'readtime', title: '抄表日期', width: 120 },
+            { field: 'mindatatime', title: '开始时间', width: 180 },
+            { field: 'maxdatetime', title: '结束时间', width: 180 },
+            { field: 'readmetertime', title: '抄表时长', width: 110, totalRow: true },
+            { field: 'meternum', title: '抄表个数', width: 110, totalRow: true },
+            { field: 'metermonth', title: '抄表月份', width: 110 },
+
+        ]], totalRow: true
+        , page: true
+        , limit: 20
+        , limits: [20, 30, 40]
+        , done: function (data) {
+            layer.close(load);
+            var num = data.count;
+            var renderdata = ShowPage(data.data, num);//渲染所有抄表员的柱状图
+            renderhtml(renderdata);
+            //轮播切换
+            $('.layadmin-carousel').each(function () {
+                var othis = $(this);
+                carousel.render({
+                    elem: this
+                    , width: '100%'
+                    , arrow: 'none'
+                    , interval: othis.data('interval')
+                    , autoplay: othis.data('autoplay') === true
+                    , trigger: (device.ios || device.android) ? 'click' : 'hover'
+                    , anim: othis.data('anim')
+                });
+            });
+            for (var i = 0; i < renderdata.length; i++) {
+                singlerender(renderdata[i]);
+
+            }
+            //
+        }
+    });
+
+    //监听查询
+    form.on('submit(numberSearch)', function (obj) {
+        var field = obj.field;
+        table.reload('dataAnalysisInfo_Table', {
+            where: {
+                'mrreadername': field.mrreadername,
+                'readDatetime01': field.readDatetime01,
+                'page': 1
+            }
+        });
+    });
+
+    //下拉框渲染
+    admin.req({
+        url: 'http://localhost:8081/render_ReaderAnalysis',
+        type: "post",
+        success: function (result) {
+            workData = result.data;
+            var strs = "";
+            strs += '<option value = "">请选择</option>'
+            for (var x in workData) {
+                strs += '<option value = "' + workData[x] + '">' + workData[x] + '</option>'
+            }
+            $("#mrreadername").html(strs);
+            form.render();
+        }
+    });
+    //监听行数据
+    /*table.on('row(dataAnalysisInfo)', function (obj) {  
+        var num = obj.data.meternum;
+        var time = obj.data.readmetertime;
+         var readtime=obj.data.readtime;
+       // ChangeNum(months, num, time);
+        console.log(obj.data);
+         
+    });*/
+
+
+    exports('ReadAnalysis', {});
+});
