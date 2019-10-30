7b5aa002c95f8675c3b32b8502131377e95e0bab
斌Ge是我 <1245972926@qq.com>
2019-10-30 15:55:00 +0800

    ok

--- src/controller/watermeterManage.js
+++ src/controller/watermeterManage.js
@@ -30,7 +30,7 @@ layui.define(['table', 'form', 'view', 'admin', 'laydate', 'upload'], function (
     });
     upload.render({
         elem: '#import'
-        , url: layui.setter.requesturl+'/api/WatermeterUserManage/PutExcel'
+        , url: layui.setter.requesturl + '/api/WatermeterUserManage/PutExcel'
         , accept: 'file' //普通文件
         , done: function (data) {
             console.log(data);
@@ -38,7 +38,7 @@ layui.define(['table', 'form', 'view', 'admin', 'laydate', 'upload'], function (
     });
     //渲染主界面表格
     table.render({
-        url: layui.setter.requesturl +'/api/WatermeterUserManage/ShowWaterUserinfo',
+        url: layui.setter.requesturl + '/api/WatermeterUserManage/ShowWaterUserinfo',
         method: 'get',
         elem: '#watermeteruser',
         cols: [[
@@ -70,10 +70,10 @@ layui.define(['table', 'form', 'view', 'admin', 'laydate', 'upload'], function (
             "telephone": obj.field.telephone,
             "usemetertype": obj.field.usemetertype,
             "username": obj.field.username,
-            "accstate":1
+            "accstate": 1
         };
         $.ajax({
-            url: layui.setter.requesturl +'/api/WatermeterUserManage/Adduser',
+            url: layui.setter.requesturl + '/api/WatermeterUserManage/Adduser',
             type: 'get',
             data: {
                 "JsonDate": JSON.stringify(sendData)
@@ -89,7 +89,7 @@ layui.define(['table', 'form', 'view', 'admin', 'laydate', 'upload'], function (
     //监听主界面的新增按钮
     form.on('submit(adduserinfo)', function (obj) {
         admin.req({
-            url: layui.setter.requesturl +'/api/WatermeterUserManage/AdduserDataShow',
+            url: layui.setter.requesturl + '/api/WatermeterUserManage/AdduserDataShow',
             type: 'post',
             data: {
 
@@ -123,7 +123,7 @@ layui.define(['table', 'form', 'view', 'admin', 'laydate', 'upload'], function (
     });
     //渲染主界面下拉框
     admin.req({
-        url: layui.setter.requesturl +'/api/WatermeterUserManage/SelectValue',
+        url: layui.setter.requesturl + '/api/WatermeterUserManage/SelectValue',
         type: 'post',
         data: {
 
@@ -165,7 +165,7 @@ layui.define(['table', 'form', 'view', 'admin', 'laydate', 'upload'], function (
     //用户查询
     form.on('submit(P_User)', function (obj) {
         table.reload('watermeteruser', {
-            url: layui.setter.requesturl +'/api/WatermeterUserManage/ShowWaterUserinfo',
+            url: layui.setter.requesturl + '/api/WatermeterUserManage/ShowWaterUserinfo',
             method: 'get',
             where: {
                 "account": obj.field.account,
@@ -182,7 +182,7 @@ layui.define(['table', 'form', 'view', 'admin', 'laydate', 'upload'], function (
     form.on('submit(Change_Sub)', function (obj) {
         admin.req({
             url: 'post',
-            type: layui.setter.requesturl +'/api/WatermeterUserManage/Changemeter',
+            type: layui.setter.requesturl + '/api/WatermeterUserManage/Changemeter',
             data: {
                 "Oldmeternum": obj.field.Oldmeternum,
                 "Newmeternum": obj.field.meternum
@@ -209,7 +209,7 @@ layui.define(['table', 'form', 'view', 'admin', 'laydate', 'upload'], function (
                 "sex": obj.data.sex
             };
             admin.req({
-                url: layui.setter.requesturl +'/api/WatermeterUserManage/EditUserInfo',
+                url: layui.setter.requesturl + '/api/WatermeterUserManage/EditUserInfo',
                 type: 'post',
                 data: {
                     "JsonDate": JSON.stringify(sendData)
@@ -227,7 +227,7 @@ layui.define(['table', 'form', 'view', 'admin', 'laydate', 'upload'], function (
             var load = layer.load(3);
             meternumber = obj.data.meternum;
             admin.req({
-                url: layui.setter.requesturl +'/api/WatermeterUserManage/ShowEditRegionDate',
+                url: layui.setter.requesturl + '/api/WatermeterUserManage/ShowEditRegionDate',
                 type: 'get',
                 data: {
                     "account": obj.data.account
@@ -300,7 +300,7 @@ layui.define(['table', 'form', 'view', 'admin', 'laydate', 'upload'], function (
                                 //监听表格的新增水表
                                 if (obj1.event == "ATable") {
                                     admin.req({
-                                        url: layui.setter.requesturl +'/api/WatermeterUserManage/showaddmeterinfo',
+                                        url: layui.setter.requesturl + '/api/WatermeterUserManage/showaddmeterinfo',
                                         type: 'post',
                                         data: {
                                             "meternum1": meternumber
@@ -326,7 +326,7 @@ layui.define(['table', 'form', 'view', 'admin', 'laydate', 'upload'], function (
                                 }
                                 if (obj1.event == "CTable") {
                                     admin.req({
-                                        url: layui.setter.requesturl +'/api/WatermeterUserManage/ShowChangemeter',
+                                        url: layui.setter.requesturl + '/api/WatermeterUserManage/ShowChangemeter',
                                         type: 'post',
                                         data: {
                                             "account": obj1.config.data[0].account
@@ -392,7 +392,7 @@ layui.define(['table', 'form', 'view', 'admin', 'laydate', 'upload'], function (
         }
         if (obj.event == "User_delete") {
             admin.req({
-                url: layui.setter.requesturl +'/api/WatermeterUserManage/DelUser',
+                url: layui.setter.requesturl + '/api/WatermeterUserManage/DelUser',
                 data: {
                     "autoaccount": obj.data.autoaccount
                 },
@@ -418,7 +418,7 @@ layui.define(['table', 'form', 'view', 'admin', 'laydate', 'upload'], function (
                 "metermodel": addobj.field.metermodel
             };
             admin.req({
-                url: layui.setter.requesturl +'/api/WatermeterUserManage/AddWaterMeter',
+                url: layui.setter.requesturl + '/api/WatermeterUserManage/AddWaterMeter',
                 type: 'post',
                 data: {
                     "JsonDate": JSON.stringify(Jsondate),
@@ -437,7 +437,7 @@ layui.define(['table', 'form', 'view', 'admin', 'laydate', 'upload'], function (
     table.on('tool(editwatermeter)', function (obj) {
         if (obj.event == "editWatermeter") {
             admin.req({
-                url: layui.setter.requesturl +'/api/WatermeterUserManage/ListData',
+                url: layui.setter.requesturl + '/api/WatermeterUserManage/ListData',
                 type: 'get',
                 data: {
 
@@ -491,7 +491,7 @@ layui.define(['table', 'form', 'view', 'admin', 'laydate', 'upload'], function (
     //监听编辑水表信息界面的提交按钮
     form.on('submit(EditWatermeterinfo_Sub)', function (obj) {
         admin.req({
-            url: layui.setter.requesturl +'/api/WatermeterUserManage/EditWaterMater',
+            url: layui.setter.requesturl + '/api/WatermeterUserManage/EditWaterMater',
             type: 'post',
             data: {
                 "JsonDate": JSON.stringify(obj.field)
