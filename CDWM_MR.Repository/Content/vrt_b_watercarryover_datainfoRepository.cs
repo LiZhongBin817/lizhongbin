cadd4de0fb812421732fa1e5ddc37a209bc6dee4
xtdf <3070137681@qq.com>
2019-11-03 17:48:09 +0800

    抄表员两年查询

---
+++ CDWM_MR.Repository/Content/vrt_b_watercarryover_datainfoRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using CDWM_MR.IRepository.Content;
+using CDWM_MR.Model.Models;
+using CDWM_MR.Repository.BASE;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CDWM_MR.Repository.Content
+{
+    public partial class vrt_b_watercarryover_datainfoRepository : BaseRepository<vrt_b_watercarryover_datainfo>, Ivrt_b_watercarryover_datainfoRepository
+    {
+    }
+}
--- CDWM_MR/Controllers/v1/AppWebCommonController.cs
+++ CDWM_MR/Controllers/v1/AppWebCommonController.cs
@@ -2,6 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CDWM_MR.IServices.Content;
+using CDWM_MR.Model;
+using CDWM_MR.Model.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -16,7 +21,70 @@ namespace CDWM_MR.Controllers.v1
     {
 
         #region 相关变量
-
+        readonly Isys_parameterServices _ParameterServices;
+        readonly Ivrt_b_watercarryover_datainfoServices _B_Watercarryover_DatainfoServices;
         #endregion
+
+        #region 构造函数
+        public AppWebCommonController(Isys_parameterServices ParameterServices, Ivrt_b_watercarryover_datainfoServices B_Watercarryover_DatainfoServices)
+        {
+            _ParameterServices = ParameterServices;
+            _B_Watercarryover_DatainfoServices = B_Watercarryover_DatainfoServices;
+        }
+        #endregion
+
+        #region 字典查询
+        [HttpPost]
+        [Route("SearchDictionary")]
+        [AllowAnonymous]
+        [EnableCors("LimitRequests")]
+        public async Task<TableModel<object>> SearchDictionary(int JsonData)
+        {
+            List<sys_parameter> data = new List<sys_parameter>();
+            var list = await _ParameterServices.Query(c => c.parametertype == JsonData);
+            var temp = list.Select(c => new {
+                parameterkey = c.parameterkey,
+                parametervalue = c.parametervalue
+            });
+            return new TableModel<object>
+            {
+                code = 0,
+                msg = "OK",
+                data = temp
+            };
+
+        }
+        #endregion
+
+        #region 抄表员两年查询
+        [HttpPost]
+        [Route("SearchReaderDate")] 
+        public async Task<TableModel<object>> SearchReaderDate(int readernameid)
+        {
+            string time,num;
+            List<vrt_b_watercarryover_datainfo> data = new List<vrt_b_watercarryover_datainfo>();
+            var list = await _B_Watercarryover_DatainfoServices.Query(c => c.readerid == readernameid);
+            Dictionary<object, object> dic = new Dictionary<object, object>(); 
+            for (int i = 0; i < list.Count; i++)
+            {
+                time = list[i].endtime.ToString();
+                string a = time.Substring(0, 7);
+                num = list[i].carrywatercount.ToString();
+                string[] b = num.Split(".");
+                dic.Add(a,b[0]); 
+                if (i > 24)
+                {
+                    break;
+                }
+            } 
+            return new TableModel<object>
+            {
+                code = 0,
+                msg = "OK",
+                data = dic
+            };
+        }
+        #endregion
+
     }
 }
