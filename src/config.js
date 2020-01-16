7830f54be545c49d54aa1c815f9acc205c810123
斌Ge是我 <1245972926@qq.com>
2020-01-16 20:44:35 +0800

    20200116

--- src/config.js
+++ src/config.js
@@ -28,7 +28,7 @@ layui.define(['laytpl', 'layer', 'element', 'util'], function (exports) {
             tokenName: 'Authorization' //自动携带 token 的字段名。可设置 false 不携带。
         }
         //向后台请求的URL地址
-        ,requesturl:'http://localhost:8081'
+        ,requesturl:'http://localhost:5432'
         //所有的按钮信息
         ,localbtnname:'localbtnlist'
         //自定义响应字段
